3cadc5682988948a9f7d0cd887d094dcb35c26e9
dbdev72 <dbdev1721@gmail.com>
2026-01-15 08:38:55 +0100

    Estableixo 3 idiomes missatge upgrader

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:upgrader/upgrader.dart';
-import '../screens/menu.dart';
-import '../screens/idioma.dart';
+import 'screens/menu.dart';
+import 'screens/idioma.dart';
 import 'styles/app_styles.dart';
 
 void main() async {
@@ -18,24 +18,18 @@ void main() async {
 
   final prefs = await SharedPreferences.getInstance();
   final bool isFirstRun = prefs.getBool('isFirstRun') ?? true;
+  final String savedLang = prefs.getString('language') ?? 'cat';
 
   await _initTracking();
 
   await MobileAds.instance.initialize();
-  MobileAds.instance.updateRequestConfiguration(
-    RequestConfiguration(
-      testDeviceIds: ["69B5C8BFFA8C4CC0CA334A51DA5028DA"],
-    ),
-  );
 
-  runApp(App(isFirstRun: isFirstRun));
+  runApp(App(isFirstRun: isFirstRun, savedLang: savedLang));
 }
 
 Future<void> _initTracking() async {
   await Future.delayed(const Duration(milliseconds: 1000));
-
   final status = await AppTrackingTransparency.trackingAuthorizationStatus;
-
   if (status == TrackingStatus.notDetermined) {
     await AppTrackingTransparency.requestTrackingAuthorization();
   }
@@ -43,18 +37,34 @@ Future<void> _initTracking() async {
 
 class App extends StatelessWidget {
   final bool isFirstRun;
-  const App({super.key, required this.isFirstRun});
+  final String savedLang;
+
+  const App({super.key, required this.isFirstRun, required this.savedLang});
+
+  String _getUpgraderCode() {
+    switch (savedLang) {
+      case 'esp':
+        return 'es';
+      case 'eng':
+        return 'en';
+      case 'cat':
+      default:
+        return 'ca';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    final String langCode = _getUpgraderCode();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Memoriumm',
       theme: AppStyles.lightTheme,
       home: UpgradeAlert(
         upgrader: Upgrader(
-          languageCode: 'ca',
-          messages: UpgraderMessages(code: 'ca'),
+          languageCode: langCode,
+          messages: UpgraderMessages(code: langCode),
           durationUntilAlertAgain: const Duration(days: 1),
         ),
         child: isFirstRun ? const Idioma() : const Menu(),
